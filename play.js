1795931c658a3a666f3bac9cd435fa4bab97e299
Sriya Bhat <sriya.s.bhat@gmail.com>
2020-09-25 02:00:33 +0000

    Implemented a code to handle user input

--- play.js
+++ play.js
@@ -5,10 +5,35 @@ const  connect  = require('./client');
 console.log('Connecting ...');
 
 let directions = ['Move: up','Move: left','Move: left','Move: down','Move: left'];
+
 const connection = connect(directions);
+
+
+
+const setUpInput = function(){
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  
+
+  stdin.on('data',(data) => {
+    if(data === '\u0003'){
+      process.exit();
+    }
+    else {
+      stdin.resume();
+    }
+  });
+  return stdin;
+}
+
+
 
 connection.on('data',(data) => {
   console.log(data);
  });
 
+ setUpInput();
+
+ 
  
